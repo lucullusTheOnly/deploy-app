d036d0a5d43916dfe72bf122ad195311d67f533e
christian <christian@christian-PC.home>
2025-10-24 16:59:12 +0200

    Changed to javascript with fetch

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,21 @@
 from flask import Flask, request, render_template_string
+import logging
 
 app = Flask(__name__)
 from flask_cors import CORS
-CORS(app)  # allows all origins
+#CORS(app)  # allows all origins
+CORS(app, supports_credentials=True)
+logging.basicConfig(level=logging.INFO)
 
-@app.route("/deploy", methods=["POST"])
-def deploy():
+@app.route("/build", methods=["POST"])
+def build():
     description = request.form.get("description")
     environment = request.form.get("environment")
     replicas = request.form.get("replicas")
-
-    return render_template_string("""
-    <div class='alert alert-success'>
-        ✅ Deploy triggered!<br>
-        Description: {{ description }}<br>
-        Environment: {{ environment }}<br>
-        Replicas: {{ replicas }}
-    </div>
-    """, description=description, environment=environment, replicas=replicas)
+    data = request.json()
+    data["status"] = "ok"
+    app.logger.info(str(data))
+    return data
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
